9f5b9f47c7d8566ed2de5fdfe4f401b613f504ec
Teppei.F <37261985+T3pp31@users.noreply.github.com>
2025-03-25 21:48:48 +0900

    csrf

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
 import os
-
+import random
 from flask import Flask, render_template, request, send_file
 from flask_wtf.csrf import CSRFProtect
 from PyPDF2 import PdfMerger
 
+# make csrf key
+csrf_key = ''.join(random.choices('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=32))
+
+# initialize Flask app and CSRF protect instance for CSRF protection
 app = Flask(__name__)
+app.config['SECRET_KEY'] = csrf_key
 csrf = CSRFProtect(app)
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
@@ -12,12 +17,15 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", csrf_token=csrf._get_csrf_token())
 
 
 @app.route("/merge", methods=["POST"])
 def merge_pdfs():
     files = request.files.getlist("pdfs")
+
+    if not csrf.validate_csrf_token(request.form.get('csrf_token')):
+        return "CSRFトークンが不正です", 403
 
     if not files:
         return "少なくとも1つのPDFをアップロードしてください", 400
